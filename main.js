e403cff92e2f0a7dad79ccf64ae9fe9bb4fdec3c
yui-kitagawa <105733455+yui-kitagawa@users.noreply.github.com>
2022-05-19 18:30:56 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+//問１
+let a = 2;
+let b = 16;
+console.log(a !== 10);
+
+if (b >= 10 && b <= 20){
+	console.log(b);
+}
+if (b % 2 === 0){
+	console.log(b);
+}
+
+//問２
+let x = 10;
+if (x >= 10 && x <= 20){
+	console.log('成功です');
+} else {
+	console.log('失敗です');
+}
+
+//問３
+let y = 0;
+if ( y % 2 === 0){
+	console.log('偶数です');
+} else {
+	console.log('奇数です');
+}
+
